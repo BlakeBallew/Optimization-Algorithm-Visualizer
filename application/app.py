115a746dc92b53dbe68eff00f90c75a1a00f2ab2
Blake Ballew <bballew2001@gmail.com>
2023-01-04 15:35:52 -0800

    worked with callback logic

--- application/app.py
+++ application/app.py
@@ -11,6 +11,7 @@ import json
 from dash import Dash, html, dcc, ctx
 from dash.dash import no_update
 import dash_loading_spinners as dls
+import dash_bootstrap_components as dbc
 import dash_daq as daq
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
@@ -93,7 +94,7 @@ app.layout = html.Div([
             html.Div([
                 dls.Roller([
                     dcc.Graph(className='contour-plot', id='contour-plot')
-                ], color='whitesmoke', show_initially=False)
+                ], color='whitesmoke')
             ], className='contour-plot-container'),
         ], className='plot-toggles-container'),
         create_contour_settings(app)
@@ -115,9 +116,7 @@ app.layout = html.Div([
 
 
 
-"""
-Callbacks
-"""
+# Serverside callbacks
 
 @app.callback(
     output = {
@@ -255,14 +254,21 @@ def update_store(recalc_btn,
     # if current_plot is None and store['2d_plot'] is not None:
     #     output.update({'app': json.loads(store['app_state'])})
     #     return output
+    # beale function: '(1.5-x+(x*y))^2+(2.25-x+(x*(y^2)))^2+(2.625-x+(x*(y^3)))^2'
+    # rosenbrock function: '(1-x)^2+100*(y-x^2)^2'
+    # compute_gradient(expression, x_range, y_range, 3, 1, accuracy)
         
     if component_triggered == 'recalc-btn':
         
+        # case where user tries to recalculate a 3d plot
+        if current_plot.get('layout', {}).get('xaxis', {}).get('range') == None:
+            return output
+        
         x_range = current_plot['layout']['xaxis']['range']
         y_range = current_plot['layout']['yaxis']['range']
-        # beale function: '(1.5-x+(x*y))^2+(2.25-x+(x*(y^2)))^2+(2.625-x+(x*(y^3)))^2'
-        # rosenbrock function: '(1-x)^2+100*(y-x^2)^2'
+        
         x_inputs, y_inputs, z = compute_zmatrix(expression, x_range, y_range, accuracy)
+        
         new_2d_fig = json.loads(store['2d_plot'])
         new_3d_fig = json.loads(store['3d_plot'])
         
@@ -274,22 +280,24 @@ def update_store(recalc_btn,
             '3d_plot': json.dumps(new_3d_fig),
         })
         
-        # compute_gradient(expression, x_range, y_range, 3, 1, accuracy)
 
         output.update({'app-store': store})
-        
         return output        
+    
         
     if component_triggered == 'reset-btn':
         pass
     
+    
     if component_triggered == 'step-btn':
         pass
+
 
     if component_triggered == 'toggle-3d':
         store.update({'toggle_3d': toggle_3d})
         output.update({'app-store': store})
         return output
+
 
     if component_triggered == 'colorscale-dropdown':
         new_2d_fig = json.loads(store['2d_plot'])
@@ -304,72 +312,57 @@ def update_store(recalc_btn,
         })
         
         output.update({'app-store': store})
-        
         return output
         
     
     if component_triggered == 'change-start':
         new_2d_fig = json.loads(store['2d_plot'])
-
         new_2d_fig['data'][0]['contours']['start'] = start
 
         store.update({
             '2d_plot': json.dumps(new_2d_fig),
         })
-
         output.update({'app-store': store})
-
         return output
 
 
     if component_triggered == 'change-stop':
         new_2d_fig = json.loads(store['2d_plot'])
-
         new_2d_fig['data'][0]['contours']['end'] = stop
-
         store.update({
             '2d_plot': json.dumps(new_2d_fig),
         })
 
         output.update({'app-store': store})
-
         return output
 
 
     if component_triggered == 'change-step':
         new_2d_fig = json.loads(store['2d_plot'])
-
         new_2d_fig['data'][0]['contours']['size'] = step
-
         store.update({
             '2d_plot': json.dumps(new_2d_fig),
         })
 
         output.update({'app-store': store})
-
         return output
 
     
     if component_triggered == 'smoothing-step-slider':
         new_2d_fig = json.loads(store['2d_plot'])
-
         new_2d_fig['data'][0]['line']['smoothing'] = smoothness
-
-
         store.update({
             '2d_plot': json.dumps(new_2d_fig),
         })
 
         output.update({'app-store': store})
-
         return output
 
 
     if component_triggered == 'accuracy-step-slider':
         x_range = current_plot['layout']['xaxis']['range']
         y_range = current_plot['layout']['yaxis']['range']
-        # beale function: '(1.5-x+(x*y))^2+(2.25-x+(x*(y^2)))^2+(2.625-x+(x*(y^3)))^2'
-        # rosenbrock function: '(1-x)^2+100*(y-x^2)^2'
+        
         x_inputs, y_inputs, z = compute_zmatrix(expression, x_range, y_range, accuracy)
         
         new_2d_fig = json.loads(store['2d_plot'])
@@ -383,158 +376,28 @@ def update_store(recalc_btn,
             '3d_plot': json.dumps(new_3d_fig),
         })
         
-        # compute_gradient(expression, x_range, y_range, 3, 1, accuracy)
-
         output.update({'app-store': store})
-        
         return output 
 
 
-@app.callback(Output(component_id='contour-plot', component_property='figure'),
-              Input(component_id='app-store', component_property='data'))
-def update_figure(store):
-    if store['toggle_3d']:
-        return json.loads(store['3d_plot'])
-    return json.loads(store['2d_plot'])
+# Clientside callbacks
 
-
-
-
-"""
-
-@app.callback(Output(component_id='contour-plot', component_property='figure'),
-              Output(component_id='plots', component_property='data'),
-              Input(component_id='zmatrix-btn', component_property='n_clicks'),
-              Input(component_id='colorscale-dropdown', component_property='value'),
-              Input(component_id='change-start', component_property='value'),
-              Input(component_id='change-stop', component_property='value'),
-              Input(component_id='change-step', component_property='value'),
-              Input(component_id='smoothing-step-slider', component_property='value'),
-              State(component_id='contour-plot', component_property='figure'),
-              State(component_id='user-expression', component_property='value'),
-              State(component_id='accuracy-step-slider', component_property='value'),
-              State(component_id='plots', component_property='data'))
-def recalculate_contour(btn_click, colorscale, start, stop, step, smoothness, current_plot, expression, accuracy, plots):
-    component_triggered = ctx.triggered_id
-
-    if current_plot is None:
-        initial_x, initial_y, initial_z = compute_zmatrix('x^2+y^2', [-3, 5], [-3, 5], accuracy)
-        contours_2d = {
-            'start': start,
-            'end': stop,
-            'size': step,
+app.clientside_callback(
+    """
+    function update_figure(store) {
+        if (store['toggle_3d']) {
+            return JSON.parse(store['3d_plot']);
+        } else {
+            return JSON.parse(store['2d_plot']);
         }
-        contours_3d = {**contours_2d, **{
-            'show': True,
-            'usecolormap': True,            
-        }}
-        colorbar = {
-            'nticks': 10,
-            'ticks': 'outside',
-            'tickwidth': 1,
-            'showticklabels': True,
-            'tickangle': 0,
-            'tickfont_size': 12,
-        }
-        layout_2d = {
-            'margin': {'t': 30, 'b': 0, 'l': 0, 'r': 0, 'pad': 5}, 
-            'plot_bgcolor': '#212329',
-            'paper_bgcolor': '#212329',
-            'font': {'color': 'white'},
-        }
+    }
+    """,
+    Output(component_id='contour-plot', component_property='figure'),
+    Input(component_id='app-store', component_property='data'),
+)
 
-        layout_3d_axis_config = {
-            'backgroundcolor': '#212329',
-            'gridcolor': 'white',
-            'showbackground': True,
-            'zerolinecolor': 'white',            
-        }
 
-        layout_3d = {**layout_2d, **{
-            'scene': {
-                'xaxis': layout_3d_axis_config,
-                'yaxis': layout_3d_axis_config,
-                'zaxis': layout_3d_axis_config,
-            },
-            'scene_camera_eye': {
-                'x': -1.1,
-                'y': -1.8,
-                'z': 0.9,
-            }
-        }}
-
-        fig_2d = go.Figure(
-            data = go.Contour(
-                    x = initial_x,
-                    y = initial_y,
-                    z = initial_z,
-                    line_smoothing=smoothness,
-                    colorscale='Electric',
-                    contours = contours_2d,
-                    colorbar = colorbar,
-            ),
-            layout = layout_2d
-        )
-
-        fig_3d = go.Figure(
-            data = go.Surface(
-                    x = initial_x,
-                    y = initial_y,
-                    z = initial_z,
-                    # line_smoothing=smoothness,
-                    colorscale='Electric',
-                    contours_z = contours_3d,
-                    # colorbar = colorbar,
-            ),
-            layout = layout_3d,
-        )
-      
-        plots.update({'2d': fig_2d.to_json(),
-                      '3d': fig_3d.to_json()})
-
-        return fig_2d, plots
-
-    # print(json.dumps(current_plot['data'][0]['line']['smoothing'], indent=3))
-
-    if component_triggered == 'zmatrix-btn':
-
-        # update is triggered by updating x, y, or z values
-        x_range = current_plot['layout']['xaxis']['range']
-        y_range = current_plot['layout']['yaxis']['range']
-
-        # beale function: '(1.5-x+(x*y))^2+(2.25-x+(x*(y^2)))^2+(2.625-x+(x*(y^3)))^2'
-        # rosenbrock function: '(1-x)^2+100*(y-x^2)^2'
-        x_inputs, y_inputs, z = compute_zmatrix(expression, x_range, y_range, accuracy)
-        current_plot['data'][0]['x'] = x_inputs
-        current_plot['data'][0]['y'] = y_inputs
-        current_plot['data'][0]['z'] = z
-
-        compute_gradient(expression, x_range, y_range, 3, 1, accuracy)
-
-        return current_plot, plots
-
-    if component_triggered == 'change-start' and start is not None:
-        current_plot['data'][0]['contours']['start'] = start
-
-    if component_triggered == 'change-stop' and stop is not None:
-        current_plot['data'][0]['contours']['end'] = stop
-        
-    if component_triggered == 'change-step' and step is not None:
-        current_plot['data'][0]['contours']['size'] = step
-
-    if component_triggered == 'colorscale-dropdown' and colorscale:
-        current_plot['data'][0]['colorscale'] = colorscale
-
-    if component_triggered == 'smoothing-step-slider':
-        current_plot['data'][0]['line']['smoothing'] = smoothness
-
-    return current_plot, plots
-
-"""
-
-"""
-Entry point into the application
-"""
+# app
 
 if __name__ == '__main__':
     app.run_server(debug=True)
